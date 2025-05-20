920b14c43d4a04fc0a74fc39da6290fdf3be812a
Jacky Chen <75767494+jackychen19@users.noreply.github.com>
2025-05-19 20:34:35 -0500

    Time: 20 ms (53%), Space: 18 MB (35.32%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -10,19 +10,19 @@ class Solution:
             
             if total > target:
                 return
-
+            
+            # build paths
             for i in range(start, len(candidates)):
-                # skip duplicates at the same level
+                # skip duplicates
                 if i > start and candidates[i] == candidates[i - 1]:
                     continue
                 
-                # prune
-                if total + candidates[i] > target:
-                    break
-                
-                path.append(candidates[i]) # add
+                # add to paths
+                path.append(candidates[i])
                 backtrack(i + 1, path, total + candidates[i])
-                path.pop() # backtrack
+                
+                # skip combo
+                path.pop()
             
-        backtrack(0, [], 0)
+        backtrack(0,[], 0)
         return res
