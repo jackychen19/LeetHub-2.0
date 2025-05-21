6f9f54500723fc45c7c9d0247581dfbd899a018f
Jacky Chen <75767494+jackychen19@users.noreply.github.com>
2025-05-21 14:21:20 -0500

    Time: 0 ms (100%), Space: 17.9 MB (81.22%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        def backtrack(start=0):
-            # If we've fixed all positions, add a copy of nums to result
-            if start == len(nums):
-                result.append(nums[:])
-                return
-            
-            for i in range(start, len(nums)):
-                # Swap current element with the start
-                nums[start], nums[i] = nums[i], nums[start]
-                # Recurse for the next element
-                backtrack(start + 1)
-                # Backtrack (undo the swap)
-                nums[start], nums[i] = nums[i], nums[start]
+        if len(nums) == 0:
+            return [[]]
 
-        backtrack()
-        return result
+        perms = self.permute(nums[1:]) # excludes first number
+        res = []
+        
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+        
+        return res
