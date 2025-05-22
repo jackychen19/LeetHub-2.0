2236d84863e9332e806c7678401d1a219e9948c4
Jacky Chen <75767494+jackychen19@users.noreply.github.com>
2025-05-22 11:24:03 -0500

    Time: 0 ms (100%), Space: 18 MB (33.61%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,8 +2,8 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         if len(nums) == 0:
             return [[]]
-
-        perms = self.permute(nums[1:]) # excludes first number
+        
+        perms = self.permute(nums[1:])
         res = []
         
         for p in perms:
